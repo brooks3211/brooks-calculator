33004e8f15288b295b617b3b2bbbcff5bd56618a
brookscode <chirwajames831@gmail.com>
2025-07-14 12:16:10 -0700

    Add files via upload

---
+++ james-final calculator/service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'jc-calculator-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
